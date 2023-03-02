32c1d259f13dbcfd33853b4304bc35d733171d9b
Yuxin Song <ys385@duke.edu>
2023-03-01 20:18:20 -0500

    finalize

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import requests
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
-@app.route('/<zipcode>')
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/zip/<zipcode>')
 def get_city_state(zipcode):
     url = 'https://api.zippopotam.us/us/'+str(zipcode)
     response = requests.get(url)
@@ -13,10 +17,6 @@ def get_city_state(zipcode):
     state = data['places'][0]['state abbreviation']
     city_state = f"{city}, {state}"
     return jsonify({'city_state': city_state})
-
-# @app.route('/')
-# def get_index(zipcode):
-#     return "Welcome to the Zipcode API!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
